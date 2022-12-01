fce7981ad7366e3eb1c442488a085a5bb64f857a
colosium <640112418103@bru.ac.th>
2022-12-01 16:21:10 +0700

    LAB4

---
+++ LAB4/www/index.php
@@ -0,0 +1,34 @@
+<?php
+echo "HELLO PHP !!";
+
+// phpinfo();
+
+$no = 10;
+$name = "aom";
+
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Dang</td>
+        </tr>
+        <?php
+    for($i = 2; $i <5 ; $i++){
+    ?>
+          <tr>
+            <td><?php echo $i ?></td>
+            <td><?= $name ?></td>
+        </tr>
+    <?php       
+    }
+    ?>    
+    </tbody>
+</table>
